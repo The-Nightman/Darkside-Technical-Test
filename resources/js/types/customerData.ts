fc247e5d449e80d71c680480c663b28b2c049668
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-08-13 20:56:06 +0100

    add CustomerData interface type

---
+++ resources/js/types/customerData.ts
@@ -0,0 +1,15 @@
+export interface CustomerData {
+  id: number | null;
+  name: string;
+  email: string;
+  phone: string;
+  house_number: string;
+  address_1: string;
+  address_2: string;
+  postcode: string;
+  city: string;
+  state: string;
+  country: string;
+  rating: string;
+  rating_manual: boolean;
+}
