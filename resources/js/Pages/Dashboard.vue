245317945cd5595e7ad8d9b267cff0e087fef7c5
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-08-13 17:37:08 +0100

    add dashboard view with darkmode styles and list of customers pending pagination

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,20 +1,53 @@
 <script setup lang="ts">
+import CustomerDashboardCard from '@/Components/CustomerDashboardCard.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+
+defineProps<{
+    customers: Array<any>;
+}>();
+
 </script>
 
 <template>
+
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>
+            <h1 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h1>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+                <div class="px-2 py-6 sm:px-6 bg-white dark:bg-gray-700 overflow-hidden shadow-sm sm:rounded-lg">
+                    <h2 class="text-gray-900 dark:text-gray-100">Customers</h2>
+                    <div class="mt-6 border border-gray-300 dark:border-gray-600 rounded-md">
+                        <div
+                            class="p-1 border-b border-gray-300 dark:border-gray-600 grid grid-cols-[min-content_auto_20%_min-content] md:grid-cols-[min-content_auto_25%_15%_min-content] lg:grid-cols-[min-content_25%_15%_auto_15%_min-content] gap-2 sm:gap-4 dark:text-gray-300">
+                            <div class="w-10" />
+                            <div class="flex items-center">
+                                Name
+                            </div>
+                            <div class="hidden md:flex items-center">
+                                Phone
+                            </div>
+                            <div class="hidden lg:flex items-center">
+                                Email
+                            </div>
+                            <div class="flex items-center">
+                                Rating
+                            </div>
+                            <div class="w-10" />
+                        </div>
+                        <ul>
+                            <li class="border-b last:border-0 border-gray-300 dark:border-gray-600"
+                                v-for="customer in customers" :key="customer.id">
+                                <CustomerDashboardCard :customer="customer" />
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- Implement Pagination Here -->
                 </div>
             </div>
         </div>
