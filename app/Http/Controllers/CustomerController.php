c2fbce64fd856427266659687fca960f4c768905
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-08-13 20:24:00 +0100

    add route for GET customer by id to auth protected routes

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CustomerData;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class CustomerController extends Controller
+{
+    public function show(Request $request)
+    {
+        // cast request id to key-value pair array
+        // without this validation will fail and customer cannot be retrieved even without validation
+        // i do not know why but it will just redirect back to dashboard
+        $request->merge(['id' => $request->route('id')]);
+
+        // Validate request
+        $request->validate([
+            'id' => [
+                'required',
+                'integer',
+                'exists:customer_data,id'
+            ]
+        ]);
+
+        // Fetch customer details by id
+        $customer = CustomerData::findOrFail($request->id);
+        return Inertia::render('Customer', [
+            'customer' => $customer,
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProfileController;
 use App\Models\CustomerData;
 use Illuminate\Foundation\Application;
@@ -30,6 +31,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/customer/{id}', [CustomerController::class, 'show'])->name('customer.show');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
