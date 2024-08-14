4872ee0bbf2938ad0b3a2c33ffaf6eca0c472b5d
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-08-14 01:28:23 +0100

    add button to Dashboard to create new customers

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,7 +2,7 @@
 import CustomerDashboardCard from '@/Components/CustomerDashboardCard.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { CustomerCardData } from '@/types/customerCardData';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 
 defineProps<{
     customers: Array<CustomerCardData>;
@@ -48,6 +48,9 @@ defineProps<{
                             </li>
                         </ul>
                     </div>
+                    <Link
+                        class="inline-flex items-center mt-4 px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+                        :href="route('customer.create')" as="button">add new customer</Link>
                     <!-- Implement Pagination Here -->
                 </div>
             </div>
